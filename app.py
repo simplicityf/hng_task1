386b0470d1c5e2b773a821717d4bb201a6febd8d
Azeezat Omobolanle <155020971+simplicityf@users.noreply.github.com>
2025-01-30 20:18:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def home():
     # Return JSON response containing the GitHub URL, current datetime, and email
     respone_data = {
         "email": "omobolanlehazeezat@gmail.com", 
-        "current_datetime": datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"), 
+        "current_datetime": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ"),
         "github_url": "https://github.com/simplicityf/hng_task1"
     }
     #response_data.sort() == False
